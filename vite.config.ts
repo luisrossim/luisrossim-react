469d459d8d99e7032c8d861a862eac41e6497ff0
luisrossim <luisrossim23@gmail.com>
2024-12-20 00:59:46 -0300

    1.4

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/luisrossim-react',
+  base: '/portfolio',
   resolve: {
     alias: {
       '@': '/src',
